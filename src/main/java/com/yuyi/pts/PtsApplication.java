08e25fafa7bb2c142c244934c2c0eada7d85aeb5
greyson <ouyangguanling@gmail.com>
2021-04-09 16:49:49 +0800

    创建项目

---
+++ src/main/java/com/yuyi/pts/PtsApplication.java
@@ -0,0 +1,13 @@
+package com.yuyi.pts;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description
+ *
+ * @author greyson
+ * @since 2021/4/9
+ */
+@SpringBootApplication
+public class PtsApplication {
+}
