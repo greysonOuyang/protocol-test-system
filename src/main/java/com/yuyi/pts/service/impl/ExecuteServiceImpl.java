a0245b95142d9003f31fb719219726b50e048ea5
greyson <ouyangguanling@gmail.com>
2021-04-12 23:27:28 +0800

    执行主类ExecuteService

--- src/main/java/com/yuyi/pts/service/impl/ExecuteServiceImpl.java
+++ src/main/java/com/yuyi/pts/service/impl/ExecuteServiceImpl.java
@@ -1,7 +1,69 @@
-package com.yuyi.pts.service.impl;/**
+package com.yuyi.pts.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.yuyi.pts.common.util.JvmMetricsUtil;
+import com.yuyi.pts.common.util.ResultEntity;
+import com.yuyi.pts.common.vo.request.RequestDataDto;
+import com.yuyi.pts.netty.NettyClient;
+import com.yuyi.pts.service.ExecuteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+
+import static com.yuyi.pts.common.util.ResultEntity.successWithData;
+
+/**
  * description
  *
  * @author greyson
  * @since 2021/4/12
- */public class ExecuteServiceImpl {
+ */
+@Service
+@Slf4j
+public class ExecuteServiceImpl implements ExecuteService {
+
+
+    @Autowired
+    private NettyClient nettyClient;
+
+    @Override
+    public void execute(WebSocketSession session,  RequestDataDto dataContent) {
+
+        JSONObject result = new JSONObject();
+        result.put("processors", JvmMetricsUtil.availableProcessors());
+        result.put("totalMemory", JvmMetricsUtil.totalMemory());
+        result.put("maxMemory", JvmMetricsUtil.maxMemory());
+        result.put("freeMemory", JvmMetricsUtil.freeMemory());
+        log.info("执行发送信息给客户端-->当前服务器性能:" + result);
+        ResultEntity<JSONObject> jsonObjectResultEntity = successWithData(result);
+        String metrix = JSON.toJSONString(jsonObjectResultEntity);
+        try {
+            session.sendMessage(new TextMessage(metrix));
+            startTest(session, dataContent);
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 真正执行测试的地方
+     *
+     * @param session
+     * @param dataContent
+     */
+    private void startTest(WebSocketSession session, RequestDataDto dataContent) {
+        String host = dataContent.getHost();
+        Integer port = dataContent.getPort();
+        // TODO SSL证书校验
+        nettyClient.setHost(host);
+        nettyClient.setPort(port);
+        nettyClient.start();
+    }
+
 }
