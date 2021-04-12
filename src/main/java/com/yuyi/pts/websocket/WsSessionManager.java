0c585880d95506366630ab5dc98ba1891ba682b0
greyson <ouyangguanling@gmail.com>
2021-04-12 16:01:57 +0800

    websocket结构初始化

--- src/main/java/com/yuyi/pts/websocket/WsSessionManager.java
+++ src/main/java/com/yuyi/pts/websocket/WsSessionManager.java
@@ -1,4 +1,4 @@
-package com.yuyi.pts.websocket.springboot;
+package com.yuyi.pts.websocket;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.WebSocketSession;
---
+++ src/main/java/com/yuyi/pts/websocket/handler/HandlerDispatcher.java
@@ -0,0 +1,7 @@
+package com.yuyi.pts.websocket.handler;/**
+ * description
+ *
+ * @author greyson
+ * @since 2021/4/12
+ */public class HandlerDispatcher {
+}
