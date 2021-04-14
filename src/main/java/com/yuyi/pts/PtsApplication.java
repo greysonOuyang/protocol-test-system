4b17eb1e18e14140c2b6e2daa611dd5cb792c367
greyson <ouyangguanling@gmail.com>
2021-04-14 11:27:44 +0800

    删除无用页面

--- src/main/java/com/yuyi/pts/PtsApplication.java
+++ src/main/java/com/yuyi/pts/PtsApplication.java
@@ -1,7 +1,5 @@
 package com.yuyi.pts;
 
-import com.yuyi.pts.service.impl.NettyMessageServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
