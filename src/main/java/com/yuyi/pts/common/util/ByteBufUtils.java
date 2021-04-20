72e77f526c0ffdf041a3ac6144c9e0e15768ce91
wuzhonglu <wuzhonglu@csii.com.cn>
2021-04-20 10:03:08 +0800

    wzl 编码问题

--- src/main/java/com/yuyi/pts/common/util/ByteBufUtils.java
+++ src/main/java/com/yuyi/pts/common/util/ByteBufUtils.java
@@ -56,4 +56,37 @@ public class ByteBufUtils {
         }
         return result;
     }
+
+    /**
+     * 16进制的字符串表示转成字节数组
+     *
+     * @param  str    16进制格式的字符串，注意字符之间不要有空格
+     * @return    转换后的字节数组
+     */
+    public static boolean isEmpty( String str ){
+        if( str == null || str.equals( "" ) ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static byte[] toByteArray(String hexString){
+        if( isEmpty( hexString ) ){
+            throw new IllegalArgumentException("this hexString must not be empty");
+        }
+
+        hexString = hexString.toLowerCase();
+        final byte[] byteArray = new byte[hexString.length() / 2];
+        int k = 0;
+
+        for( int i = 0; i < byteArray.length; i++ ){
+            //因为是16进制，最多只会占用4位，转换成字节需要两个16进制的字符，高位在先
+            byte high = (byte) (Character.digit(hexString.charAt(k), 16) & 0xff);
+            byte low = (byte) (Character.digit(hexString.charAt(k + 1), 16) & 0xff);
+            byteArray[i] = (byte) ( high << 4 | low);
+            k += 2;
+        }
+        return byteArray;
+    }
 }
