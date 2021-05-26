cd5a367d1c4bfdace95c7c1b861b397e54020a00
JoyWu <2546152549@qq.com>
2021-05-26 16:49:49 +0800

    导入存入数据库

--- src/main/java/com/yuyi/pts/controller/ExcelUtilController.java
+++ src/main/java/com/yuyi/pts/controller/ExcelUtilController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -65,13 +64,13 @@ public class ExcelUtilController {
     public void downLoadStuInfoExcel(HttpServletResponse response, HttpServletRequest request) {
         try {
             //获取要下载的模板名称
-            String fileName = "static/计划信息.xlsx";
+            String fileName = "计划信息.xlsx";
             //设置要下载的文件的名称
             response.setHeader("Content-disposition", "attachment;fileName=" + fileName);
             //通知客服文件的MIME类型
             response.setContentType("application/vnd.ms-excel;charset=UTF-8");
             //获取文件的路径
-            ClassPathResource classPathResource = new ClassPathResource("static/计划信息.xlsx");
+            ClassPathResource classPathResource = new ClassPathResource("计划信息.xlsx");
             String filePath  = classPathResource.getPath();
             InputStream input = classPathResource.getInputStream();
             OutputStream out = response.getOutputStream();
