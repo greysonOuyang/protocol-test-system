2ecbf9941be672977ac542386270438bb446802f
greyson <ouyangguanling@gmail.com>
2021-08-03 18:09:53 +0800

    配置表

--- src/main/java/com/yuyi/pts/controller/ConfigController.java
+++ src/main/java/com/yuyi/pts/controller/ConfigController.java
@@ -5,11 +5,9 @@ import com.yuyi.pts.entity.ConfigEntity;
 import com.yuyi.pts.repository.ConfigRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -29,20 +27,18 @@ public class ConfigController {
 
     /**
      * 新增或修改配置
-     * @param map
+     * @param configList
      * @return
      */
     @PostMapping("/save")
-    public String saveRequestConfig(@RequestBody Map<String, Object> map) {
-        Integer id = (Integer) map.get("id");
-        List<ConfigEntity> configList = (ArrayList) map.get("configList");
-        configList.forEach(
-                item-> {
-                    item.setRequestId(id);
-                }
-        );
+    public String saveRequestConfig(@RequestBody List<ConfigEntity> configList) {
         configRepository.saveAll(configList);
         return ResultUtil.successWithNothing();
+    }
+
+    @GetMapping("/find/list/requestId")
+    public List<ConfigEntity> findList(Integer requestId) {
+        return configRepository.findConfigEntitiesByRequestId(requestId);
     }
 
 }
